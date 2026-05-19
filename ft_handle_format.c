607ef3839a579c8d00415bddd6b5c63114452537
Heider Fandino <hfandino@cbr8s1.42barcelona.com>
2026-05-19 10:51:20 +0200

    fix ft_printf after merge

---
+++ ft_handle_format.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_handle_format.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfandino <hfandino@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 18:00:47 by hfandino          #+#    #+#             */
+/*   Updated: 2026/05/16 13:38:08 by hfandino         ###   ########.fr       */
+/*   Updated: 2026/05/15 17:53:42 by hfandino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_handle_format(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (ft_print_char(va_arg(args, int)));
+	else if (specifier == 's')
+		return (ft_print_str(va_arg(args, char *)));
+	else if (specifier == 'd' || specifier == 'i')
+		return (ft_print_nbr(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (ft_print_unsigned(va_arg(args, unsigned int)));
+	else if (specifier == '%')
+		return (ft_print_char('%'));i
+	else if (specifier == 'x')
+		return (ft_print_hexa(va_arg(args, unsigned int), "0123456789abcdef"));
+	else if (specifier == 'X')
+		return (ft_print_hexa(va_arg(args, unsigned int), "0123456789ABCDEF"));
+	else if (specifier == 'p')
+	return (ft_print_ptr((unsigned long)va_arg(args, void *)));
+	else
+		return (0);
+}
---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfandino <hfandino@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 16:46:46 by hfandino          #+#    #+#             */
+/*   Updated: 2026/05/16 13:38:29 by hfandino         ###   ########.fr       */
+/*   Updated: 2026/05/15 17:15:25 by hfandino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stddef.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	t_print_hexa(unsigned int nb, char *base);
+int	ft_print_ptr(unsigned long ptr);
+int	ft_print_hex_long(unsigned long nb, char *base);
+int	ft_print_char(char c);
+int	ft_print_str(char *str);
+int	ft_handle_format(char specifier, va_list args);
+int	ft_printf(char const	*format, ...);
+int	ft_print_nbr(int nb);
+#endif
